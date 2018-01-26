967e172f928d1efbb8cf3c62b3bca1115bd8b00d
sdobon <sebastiandobon2@gmail.com>
2018-01-25 23:57:17 -0600

    string manip

--- minet-netclass-w18/src/apps/http_client.cc
+++ minet-netclass-w18/src/apps/http_client.cc
@@ -25,7 +25,7 @@ int main(int argc, char * argv[]) {
     char * bptr = NULL;
     char * bptr2 = NULL;
     char * endheaders = NULL;
-    string request = "";
+    char * request = "";
 
     struct timeval timeout;
     fd_set set;
