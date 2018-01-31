e14b54e59b7dee442ea74190d0ef8a88019a6238
denizsagnaklar <denizsagnaklar@gmail.com>
2018-01-31 15:16:25 -0600

    Update http_client.cc

--- minet-netclass-w18/src/apps/http_client.cc
+++ minet-netclass-w18/src/apps/http_client.cc
@@ -111,7 +111,7 @@ if (FD_ISSET(sockfd, &set)) {
 		clen = strstr(buf, "Content-Length:");
 		headers = strtok(clen, "\n");
 		meslen = atoi(headers);
-		mes = buf.mid(10 - meslen);
+		mes = buf.Right(meslen);
 		printf ("%s\n", mes);
 	} else {
 		printf("%s\n", buf);
