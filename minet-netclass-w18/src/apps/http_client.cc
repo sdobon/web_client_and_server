314757921d0ce10a8b904ab5f8f248e3c4123e84
denizsagnaklar <denizsagnaklar@gmail.com>
2018-01-31 16:28:34 -0600

    Update http_client.cc

--- minet-netclass-w18/src/apps/http_client.cc
+++ minet-netclass-w18/src/apps/http_client.cc
@@ -115,7 +115,7 @@ if (FD_ISSET(sockfd, &set)) {
 	printf ("%s\n", clen);
 	printf ("%d\n", clenint);
 	if (ecint == 200) {
-		mes = buf + 300;
+		mes = buf + 200;
 		printf ("%s\n", mes);
 	} else {
 		printf("%s\n", buf);
